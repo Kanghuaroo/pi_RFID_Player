c3c83b76c544faa19a369ca25d33d20e271afa1b
David Miller - Pi <5035dcmiller@gmail.com>
2020-02-01 22:29:29 -0500

    Turned LCD_Screen into a class

--- LCD_Screen.py
+++ LCD_Screen.py
@@ -1,10 +1,24 @@
-from RPLCD import CharLCD
+from RPLCD.gpio import CharLCD
+from RPi import GPIO
 from time import sleep
 
-lcd = CharLCD(cols=16, rows=2, pin_rs=37, pin_e=35, pins_data=[33,31,29,36])
 
-lcd.write_string(u"Hello World!")
+class LCD_Screen:
 
-sleep(5)
+    def __init__(self):
+        lcd = CharLCD(cols=16, rows=2, pin_rs=37, pin_e=35, 
+                pins_data=[33,31,29,36],numbering_mode=GPIO.BOARD)
+    
+    #Basic write to LCD Screen
+    def write(self, Str):
+        self.lcd.write_string(Str)
+        sleep(5)
+        self.lcd.clear()
 
-lcd.clear()
+    #Print a song to the LCD Screen
+    def writeSong(self, song, time):
+        self.lcd.clear()
+        if len(song) > 16:
+            song = song[:16]
+        self.lcd.write_string(song)
+        self.lcd.write_string(time)
