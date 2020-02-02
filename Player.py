c66a41c54faf65df39ebdb9eae425228bb124735
David Miller - Pi <5035dcmiller@gmail.com>
2020-02-02 03:23:07 -0500

    One Player, scope, and OAuth

--- Player.py
+++ Player.py
@@ -4,11 +4,23 @@ import sys
 from time import sleep
 import RPi.GPIO as GPIO
 from mfrc522 import SimpleMFRC522
+import spotipy
+import spotipy.util as util
 from PlaybackModifier import PlaybackModifier
 from Reader import Reader
 
-reader = Reader()
-playback = PlaybackModifier()
+ID='bf8057981097462a95729337360b7f03'
+SECRET='264737bc4ad443429635d93babefc597'
+URI='https://localhost/'
+
+scope = 'user-read-playback-state user-modify-playback-state'
+token = util.prompt_for_user_token(username='lyirk',
+        scope=scope,client_id=ID,client_secret=SECRET, redirect_uri=URI)
+
+sp = spotipy.Spotify(auth=token)
+
+reader = Reader(sp)
+playback = PlaybackModifier(sp)
 
 rfid = SimpleMFRC522()
 
--- Reader.py
+++ Reader.py
@@ -5,18 +5,9 @@ from PlayerWriter import PlayerWriter
 
 class Reader:
 
-    def __init__(self):
+    def __init__(self, sp):
         self.lcd = LCD_Screen()
-        
-        ID='1138b697263c4280bf7c63bbb3d9e698'
-        SECRET='447c1235c85c4f76ad7b676e7300cf70'
-        URI='https://localhost/'
-        
-        scope = 'user-read-playback-state'
-        songToken = util.prompt_for_user_token(username='lyirk',
-                scope=scope,client_id=ID,client_secret=SECRET, redirect_uri=URI)
-        
-        self.readerPlayer = spotipy.Spotify(auth=songToken)
+        self.readerPlayer = sp
 
     def update_LCD(self):
         self.lcd.writeSong(self.readerPlayer.current_playback()['item']['name'])
