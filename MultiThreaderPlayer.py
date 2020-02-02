c1905ef0592638fed9efc0fa6552379e5ab60422
David Miller - Pi <5035dcmiller@gmail.com>
2020-02-02 10:28:14 -0500

    Multithreading atempt

---
+++ MultiThreaderPlayer.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python
+
+#import sys
+from multiprocessing import Process
+
+from time import sleep
+import RPi.GPIO as GPIO
+from mfrc522 import SimpleMFRC522
+import spotipy
+import spotipy.util as util
+from PlaybackModifier import PlaybackModifier
+from Reader import Reader
+
+#reads buttons and updates LCD
+def readerThread():
+    while True:
+        if GPIO.input(button1) == GPIO.HIGH:
+            print('skip')
+            playback.skip()
+        elif GPIO.input(button2) == GPIO.HIGH:
+            print('pasue')
+            playback.pause()
+        elif GPIO.input(button3) == GPIO.HIGH:
+            print('rewind time')
+            playback.rewind()
+        sleep(1)
+        reader.update_LCD()
+
+#takes RFID Scanner
+def scanThread():
+    while True:
+        print("Scan a Card")
+        text = rfid.read()
+        if GPIO.input(button1) == GPIO.HIGH or GPIO.input(button2) == GPIO.HIGH or GPIO.input(button3) == GPIO.HIGH:
+            print("Wait...")
+            reader.write_to_LCD("Please Wait...")
+            sleep(1)
+
+        if (GPIO.input(button1) == GPIO.HIGH and GPIO.input(button2) == GPIO.HIGH) or (GPIO.input(button2) == GPIO.HIGH and GPIO.input(button3) == GPIO.HIGH):
+            print("Stop Holding")
+            reader.write_to_LCD("Stop Holding")
+            sleep(1)
+            reader.write_to_card(rfid, Pins)
+        else:
+            playback.changeSong(text)
+
+
+#init of pin stuff
+#check GPIO pin mode
+if GPIO.getmode() == 10:
+    #GPIO.BOARD
+    button1 = 40
+    button2 = 38
+    button3 = 32
+elif GPIO.getmode() == 11:
+    #GPIO.BCM
+    button1 = 21
+    button2 = 20
+    button3 = 12
+else:
+    GPIO.setmode(GPIO.BOARD)
+    button1 = 40
+    button2 = 38
+    button3 = 32
+Pins = [button1, button2, button3]
+rfid = SimpleMFRC522()
+GPIO.setup(button1, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(button2, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(button3, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+ID='bf8057981097462a95729337360b7f03'
+SECRET='264737bc4ad443429635d93babefc597'
+URI='https://localhost/'
+
+scope = 'user-read-playback-state user-modify-playback-state'
+token = util.prompt_for_user_token(username='lyirk',
+        scope=scope,client_id=ID,client_secret=SECRET, redirect_uri=URI)
+
+#init of player and classes
+sp = spotipy.Spotify(auth=token)
+reader = Reader(sp)
+playback = PlaybackModifier(sp)
+
+
+try:
+    reader.update_LCD()
+    
+    while True:
+        p = Process(target=readerThread)
+        q = Process(target=scanThread)
+        q.start()
+        p.start()
+        p.join()
+        q.joiin()
+except KeyboardInterrupt:
+    GPIO.cleanup()
+finally:
+    GPIO.cleanup()
